d23058f7649d540ef6d90945db608bac2738e87c
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-15 22:06:10 +0200

    init monorepo

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "tsconfig.json",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "prettier", "standard", "jest"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:import/typescript",
+		"standard",
+		"prettier",
+		"prettier/@typescript-eslint",
+		"plugin:react/recommended",
+		"prettier/standard",
+		"prettier-standard/lib/base",
+		"plugin:jest/recommended",
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	rules: {
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+	},
+};
